3f4b48b8d7167b9610ffb29f0f7a02c32e0cee62
yasmine <yasmine.ezequiel@gmail.com>
2019-09-10 17:10:32 +0200

    Adds refactor click button to world.js
    Adds contact form element in app.js.

--- features/support/world.js
+++ features/support/world.js
@@ -22,8 +22,8 @@ class AddressBookWorld {
 
     expect(actualContent).to.be.eq(expectedContent)
   }
-  async clickOnAddContactBtn() {
-    const btnSelector = '.add-contact'
+  async clickOnAddContactBtn(btnName) {
+    const btnSelector = this.btnSelectorFromName(btnName.toLowerCase())
     await this.page.waitForSelector(btnSelector)
     await this.page.click(btnSelector)
   }
@@ -32,6 +32,19 @@ class AddressBookWorld {
     await this.page.waitForSelector(inputSelector)
     this.inputElement = await this.page.$(inputSelector)
     await this.inputElement.type(content)
+  }
+  btnSelectorFromName(btnName) {
+    switch (btnName) {
+      case 'add contact':
+        return '.add-contact'
+        break
+      case 'save contact':
+        return '.save-contact'
+        break
+      default:
+        throw `${btnName} button is not defined`
+        break
+    }
   }
 }
 
--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
-console.log("Hello Students!");
+document.addEventListener('DOMContentLoaded', () => {
+  // Select form object from the DOM
+  const addContactForm = document.querySelector('.new-contact-form')
+
+  // Register an event to listen for form submission
+  addContactForm.addEventListener('submit', event => {
+    // Disable default behavior when submitting form
+    event.preventDefault()
+
+    // Get all inputs elements from the form
+    const {
+      name,
+      email,
+      phone,
+      company,
+      notes,
+      twitter,
+    } = addContactForm.elements
+
+    // Create contact object
+    const contact = {
+      id: Date.now(),
+      name: name.value,
+      email: email.value,
+      phone: phone.value,
+      company: company.value,
+      notes: notes.value,
+      twitter: twitter.value,
+    }
+
+    console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
+  })
+})
